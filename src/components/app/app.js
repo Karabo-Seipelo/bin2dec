484d3b89ae5d5e2c064f6ece340d42d84013945a
Karabo Seipelo <1788640+Souldier@users.noreply.github.com>
2019-08-24 20:58:00 +0200

    User can enter up to 8 binary digits in one input field (#10)
    * added imaged upload
    
    * webpack update
    
    * User can enter up to 8 binary digits in one input field

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,36 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
+import bin2dec from '../../services/bin2dec';
 
 const App = () => {
+    const [binary, setBinary] = useState();
+    const [error, setError] = useState(null);
+    const [output, setOutput] = useState();
+
+    const onChangeHander = (e) => {
+        const value = e.target.value;
+        const patt = /[1|0]{0,8}/g;
+
+        if (patt.test(value)) {
+            setBinary(bin2dec(value));
+            setError(null);
+        } else {
+            setError('Please enter binary values');
+        }
+    };
+
+    const onClickHandler = (e) => {
+        setOutput(binary);
+    };
+
     return (
         <div className="full-screen">
             <div>
-                <h1>React Page </h1>
+                <h1>Bnary to Decimal </h1>
+                <input type="string" maxLength="8" onChange={onChangeHander} />
+                <button onClick={onClickHandler} disabled={error ? true : false}>
+                    Convert Binary to Decimal
+                </button>
             </div>
         </div>
     );
